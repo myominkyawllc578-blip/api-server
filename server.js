abe3c9774a8fe310a6150576a49ae2b24e2a3a6d
myominkyawllc578-blip <myominkyawllc578@gmail.com>
2026-05-11 22:54:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,26 @@
 const express = require("express");
-const fetch = require("node-fetch");
 
 const app = express();
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.post("/redirect/facebook_graph_endpoint/v24.1/:id/payout", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://graph.facebook.com/v24.1/${req.params.id}/payout?access_token=${req.query.access_token}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req.body)
-      }
-    );
-
-    const data = await response.json();
-    res.json(data);
-
-  } catch (e) {
-    res.json({
-      success: false,
-      error: e.toString()
-    });
-  }
+app.post("/redirect/facebook_graph_endpoint/:id/payout", async (req, res) => {
+  res.json({
+    success: true,
+    message: "API Connected"
+  });
 });
 
 app.post("/redirect/facebook_graph_endpoint/v24.1/:id/earning_sources", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://graph.facebook.com/v24.1/${req.params.id}/earning_sources?access_token=${req.query.access_token}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req.body)
-      }
-    );
-
-    const data = await response.json();
-    res.json(data);
-
-  } catch (e) {
-    res.json({
-      success: false,
-      error: e.toString()
-    });
-  }
+  res.json({
+    success: true,
+    _sources: [],
+    has_next_page: false
+  });
 });
 
 const PORT = process.env.PORT || 3000;
